e672d93c70c43408c7801672c3a539c20e4aed9a
Anton Chirkov <Anton@Mac-mini-Oleg.local>
2022-03-09 16:25:22 +0300

    transit2

--- tg/CustomPinField/CustomPinField.swift
+++ tg/CustomPinField/CustomPinField.swift
@@ -16,33 +16,52 @@ class CustomPinField: UIControl {
     private let hintLabel = UILabel()
     private var buttonState = true
     
-    override func draw(_ rect: CGRect) {
-        self.layer.cornerRadius = 10
-
-        print(self.backgroundView.bounds.maxY)
+    private enum Constants {
+        static let cornerRadiusMainView: CGFloat = 10
+        static let buttonHeight: CGFloat = 40
+        static let textCancel = "cancel"
+        static let textNew = "new"
+    }
     
-        
-        
+    private enum ShowHide {
+        case show, hide, hideShow
+    }
+    
+    
+    override func draw(_ rect: CGRect) {
+        self.layer.cornerRadius = Constants.cornerRadiusMainView
+        drawViewNew()
+        drawLabelNew()
+        drawBackgroundView()
+        drawCells()
+        drawTextField()
+        drawHintLabel()
+        UIView.animate(withDuration: 0.5, delay: 0, options: [.curveEaseInOut]) {
+            self.viewNew.frame.origin.y = self.viewNew.frame.origin.y + 40
+        }
+    }
+    
+    private func drawViewNew() {
         viewNew.frame = CGRect(x: self.bounds.maxX / 1.6, y: self.backgroundView.bounds.maxY - 42, width: self.bounds.maxX / 3.5, height: self.bounds.maxY / 4.5)
         viewNew.layer.borderColor = UIColor.systemGray5.cgColor
         viewNew.layer.borderWidth = 1
         viewNew.backgroundColor = .systemGray6
         viewNew.layer.cornerRadius = 5
         self.addSubview(viewNew)
-        
-        
-        
-        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap(sender:)))
+        viewNew.addGestureRecognizer(tapGesture)
+    }
+    
+    private func drawLabelNew() {
         labelNew.frame = CGRect(x: 0, y: 0, width: viewNew.bounds.maxX, height: viewNew.bounds.maxY)
         labelNew.textColor = .systemGray2
         labelNew.textAlignment = .center
         labelNew.font = UIFont(name: "Courier New", size: 23)
         labelNew.text = "new"
         viewNew.addSubview(labelNew)
-        
-        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap(sender:)))
-        viewNew.addGestureRecognizer(tapGesture)
-        
+    }
+    
+    private func drawBackgroundView() {
         backgroundView.frame = CGRect(x: 0, y: 0, width: self.bounds.maxX, height: self.bounds.maxY - 40)
         backgroundView.layer.cornerRadius = 10
         backgroundView.backgroundColor = .systemGray6
@@ -50,9 +69,9 @@ class CustomPinField: UIControl {
         backgroundView.layer.borderWidth = 2
         self.layer.masksToBounds = true
         self.addSubview(backgroundView)
-        
-        drawCells()
-        
+    }
+    
+    private func drawTextField() {
         textField.frame = CGRect(x: backgroundView.bounds.maxX / 7, y: backgroundView.bounds.maxY / 2.7, width: backgroundView.bounds.width, height: backgroundView.bounds.maxY / 2)
         textField.delegate = self
         textField.font = UIFont(name: "Optima", size: 30)
@@ -63,62 +82,18 @@ class CustomPinField: UIControl {
         textField.keyboardType = UIKeyboardType.numberPad
         textField.becomeFirstResponder()
         backgroundView.addSubview(textField)
-        
+    }
+    
+    private func drawHintLabel() {
         hintLabel.frame = CGRect(x: backgroundView.bounds.maxX / 9, y: 3, width: backgroundView.bounds.maxX / 9 * 7, height: backgroundView.bounds.maxY / 3)
         hintLabel.text = ".password"
         hintLabel.textColor = .systemGray2
         hintLabel.font = UIFont(name: "Courier New", size: 28)
         hintLabel.textAlignment = .right
         backgroundView.addSubview(hintLabel)
-        
-        UIView.animate(withDuration: 0.5, delay: 0, options: [.curveEaseInOut]) {
-            self.viewNew.frame.origin.y = self.viewNew.frame.origin.y + 40
-        }
     }
     
-    func drawViewNew() {
-        
-        
-        
-        
-        
-        
-    }
-    
-    func drawLabelNew() {
-        
-        
-        
-    }
-    
-    func drawBackgroundView() {
-        
-        
-        
-    }
-    
-    func drawTextField() {
-        
-        
-        
-    }
-    
-    func drawHintLabel() {
-    
-    
-    
-    
-    
-    }
-    
-    
-    
-    
-    
-    
-    
-    
-    func drawCells() {
+    private func drawCells() {
         for i in 0...3 {
             let width = backgroundView.bounds.maxX / 9
             let emptyView = UIView(frame: CGRect(x: CGFloat(i) * 2 * width + width, y: backgroundView.bounds.maxY / 2.5, width: width, height: backgroundView.bounds.maxY / 2.3))
@@ -128,39 +103,42 @@ class CustomPinField: UIControl {
         }
     }
     
-    @objc private func handleTap(sender: UITapGestureRecognizer) {
-        let text = buttonState ? "cancel" : "new"
-        buttonState = !buttonState
+    
+    private func animateButton(action: ShowHide) {
+       
+        var motion = -Constants.buttonHeight
+        if action == .show {
+            motion = -motion
+        }
+        
+        let text = buttonState ? Constants.textCancel : Constants.textNew
+        
         UIView.animate(withDuration: 0.5, delay: 0, options: [.curveEaseInOut]) {
-            self.viewNew.frame.origin.y = self.viewNew.frame.origin.y - 40
+            self.viewNew.frame.origin.y = self.viewNew.frame.origin.y + motion
         } completion: { _ in
-            UIView.animate(withDuration: 0.5, delay: 0, options: [.curveEaseInOut]) {
+            if action == .hideShow {
                 self.labelNew.text = text
-                self.viewNew.frame.origin.y = self.viewNew.frame.origin.y + 40
+                self.animateButton(action: .show)
             }
         }
+    }
+    
+    @objc private func handleTap(sender: UITapGestureRecognizer) {
+        
+        buttonState = !buttonState
+        animateButton(action: .hideShow)
         //self.sendActions(for: UIControl.Event.touchUpInside)
     }
-//
-//    func getSelectedIndex() -> Int {
-//        return selectedIndex
-//    }
-//
-//    func setFont(size: CGFloat) {
-//        sizeOfFont = size
-//    }
-//
-//    func setSections(labels: [String]) {
-//        arrayOfSections = labels
-//    }
-//
-//    func changeLabel(index: Int, labelText: String) {
-//        if index < arrayOfSections.count {
-//            arrayOfSections[index] = labelText
-//        }
-//    }
+    
+    func getPassword() {
+    
+        
+    }
+    
+    
+    
+    
 }
-
 
 extension CustomPinField: UITextFieldDelegate {
     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
